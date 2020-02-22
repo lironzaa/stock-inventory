6a33201089eae6a174958e1851dfaa77fdda237b
lironzaa <lironzaa@gmail>
2020-02-22 15:32:05 +0200

    css styles

--- src/app/components/stock-item-list/stock-item-card/stock-item-card.component.ts
+++ src/app/components/stock-item-list/stock-item-card/stock-item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stock-item-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-item-card.component.scss']
 })
 export class StockItemCardComponent implements OnInit {
-
+  @Input() isInStock: boolean = true;
   constructor() { }
 
   ngOnInit() {
